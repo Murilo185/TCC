7002624d8b243ccdef8cafb4df7735e51c7139d5
Murilo Mendes Pedra <pedramurilo14@gmail.com>
2024-06-14 19:45:33 -0300

    Aprimoramento da página inicial (ainda não concluido)

--- src/components/telaprincipal.jsx
+++ src/components/telaprincipal.jsx
@@ -21,42 +21,56 @@ export default function TelaPrincipal (){
                 <a href="pgProduto/camisa.jsx">
                     <img src={camisa} alt="" />
                 </a>
+                <p className="nomeProduto ">Camisa</p>
+                <p className="Descricao">A partir de <span>39</span>,90</p>
             </div>
 
             <div id="Caneca">
                 <a href="pgProduto/caneca.jsx">
                     <img src={caneca} alt="" />
                 </a>
+                <p className='nomeProduto'>Caneca</p>
+                <p className="Descricao">A partir de <span>19</span>,90</p>
             </div>
 
             <div id="Almofada">
                 <a href="pgProduto/almofada.jsx">
                     <img src={almofada} alt="" />
                 </a>
+                <p className='nomeProduto' >Almofada</p>
+                <p className="Descricao">A partir de <span>19</span>,90</p>
             </div>
 
             <div id="Caderno">
                 <a href="pgProduto/caderno.jsx">
                     <img src={caderno} alt="" />
                 </a>
+                <p className='nomeProduto'>Caderno</p>
+                <p className="Descricao">A partir de <span>19</span>,90</p>
             </div>
 
             <div id="Azulejo">
                 <a href="pgProduto/azulejo.jsx">
                     <img src={azulejo} alt="" />
                 </a>
+                <p className='nomeProduto'>Azulejo</p>
+                <p className="Descricao">A partir de <span>19</span>,90</p>
             </div>
 
             <div id="Agenda">
                 <a href="pgProduto/agenda.jsx">
                     <img src={agenda} alt="" />
                 </a>
+                <p className='nomeProduto'>Agenda</p>
+                <p className="Descricao">A partir de <span>14</span>,90</p>
             </div>
 
             <div id="Chaveiro">
                 <a href="pgProduto/chaveiro.jsx">
                     <img src={chaveiro} alt="" />
                 </a>
+                <p className='nomeProduto'>Chaveiro</p>
+                <p className="Descricao">A partir de <span>12</span>,90</p>
             </div>
         </div>
         <img src={whatsapp} alt="" id='whatsapp'/>
